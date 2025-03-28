58e9997c81c75eb8a4c97fed87357f2289ea038a
Kaleu <kaleu.depaula9@gmail.com>
2025-03-28 09:39:21 -0300

    commit

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from Classes.pessoa import Pessoa
-from Classes.produto import Produto
-from Classes.dados import BaseDeDados
+# Código comentado para saber o que foi feito anteriormente;
 
-from Association.escritor import Escritor
-from Association.caneta import Caneta
-from Association.maquina_escrever import MaquinaEscrever
+# from Association.escritor import Escritor
+# from Association.caneta import Caneta
+# from Association.maquina_escrever import MaquinaEscrever
 
-escritor = Escritor('João')
-caneta = Caneta('Bic')
-maquina = MaquinaEscrever()
+# escritor = Escritor('João')
+# caneta   = Caneta('Bic')
+# maquina  = MaquinaEscrever()
 
-escritor.ferramenta = caneta
-escritor.ferramenta.escrever() # Executa o método escrever dentro da classe Caneta
+# escritor.ferramenta = caneta
+# escritor.ferramenta.escrever() # Executa o método escrever dentro da classe Caneta
+
